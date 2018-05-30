c94645c486dda3cd103bac02acd66c299f2e5f1f
Ygal HALIMI <ygalhalimi1@gmail.com>
2018-05-30 13:37:23 +0300

    added my first commit (html/js)

--- js/ToDo.js
+++ js/ToDo.js
@@ -1,40 +1,91 @@
-class Boxes extends React.Component {
+class TodoList extends React.Component {
     constructor(props) {
         super(props);
-        this.addBox = this.addBox.bind(this);
         this.state = {
-            array: [],
-            myElement: ""
+            items: []
+        };
+        this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+    }
+
+    addItem(e) {
+        if (this._inputElement.value !== "") {
+            var newItem = {
+                text: this._inputElement.value,
+                key: Date.now()
+            };
+
+            this.setState((prevState) => {
+                return {
+                    items: prevState.items.concat(newItem)
+                };
+            });
+
+            this._inputElement.value = "";
         }
 
+        console.log(this.state.items);
+
+        e.preventDefault();
     }
 
-    addBox() {
+    deleteItem(key) {
+        var filteredItems = this.state.items.filter(function (item) {
+          return (item.key !== key);
+        });
+       
         this.setState({
-            newElement : this.state.array.push(""),
-            myElement : <div>{this.state.array.map(x=> <div className="bBox" key={x}></div>)}</div>
-        })
+          items: filteredItems
+        });
+      }      
 
-
-
-    }
     render() {
-
-
         return (
-            <div className="bigBox">
-                <button onClick={this.addBox}>Add Box</button>
-                {this.state.myElement}
+            <div className="todoListMain">
+                <div className="header">
+                    <form onSubmit={this.addItem}>
+                        <input ref={(a) => this._inputElement = a}
+                            placeholder="enter task">
+                        </input>
+                        <button type="submit">add</button>
+                    </form>
+                </div>
+                <TodoItems entries={this.state.items}
+                    delete={this.deleteItem} />
             </div>
         );
     }
-
 }
 
+class TodoItems extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.createTasks = this.createTasks.bind(this);
+    }
+    delete(key) {
+        this.props.delete(key);
+    }
+    createTasks(item) {
+        return <li onClick={() => this.delete(item.key)}
+            key={item.key}>{item.text}</li>
+    }
+
+    render() {
+        var todoEntries = this.props.entries;
+        var listItems = todoEntries.map(this.createTasks);
+
+        return (
+            <ul className="theList">
+                {listItems}
+            </ul>
+        );
+    }
+};
 
 function render() {
     ReactDOM.render(
-        <Boxes />,
+        <TodoList />,
         document.getElementById("root")
     );
 }
